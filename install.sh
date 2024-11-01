6f8947877b112c96b92f45ff569ffe62aeb9a967
Ivan Zorin <creator@localzet.com>
2024-11-01 16:13:17 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,39 +5,37 @@ green='\033[0;32m'
 yellow='\033[0;33m'
 plain='\033[0m'
 
-function LOGD() {
-    echo -e "${yellow}[DEG] $* ${plain}"
-}
-
 function LOGE() {
-    echo -e "${red}[ERR] $* ${plain}"
+    echo -e "${red} $* ${plain}"
 }
 
 function LOGI() {
-    echo -e "${green}[INF] $* ${plain}"
+    echo -e "${green} $* ${plain}"
 }
 
 cur_dir=$(pwd)
-
-[[ $EUID -ne 0 ]] && LOGE "Ошибка: Пожалуйста, запустите скрипт с root-правами! \n " && exit 1
-
-apt-get update && apt install -y -q wget curl tar tzdata
-
-echo -e "${green}Запуск...${plain}"
+LOGI "Запуск..."
 
 tag_version=$(curl -Ls "https://api.github.com/repos/localzet/Triangle/releases/latest" | grep '"tag_name":' | sed -E 's/.*"([^"]+)".*/\1/')
+
 if [[ ! -n "$tag_version" ]]; then
-    echo -e "${red}Ошибка получения версии Triangle, возможно, это связано с ограничениями API Github, попробуйте позже${plain}"
+    LOGE "Ошибка получения версии Triangle, возможно, это связано с ограничениями API Github, попробуйте позже"
     exit 1
 fi
-echo -e "Получена версия Triangle: ${tag_version}, запуск установки..."
+
+echo -e "Получена версия Triangle: ${tag_version}, загрузка..."
 wget -N --no-check-certificate -O ${cur_dir}/${tag_version}.tar.gz https://github.com/localzet/Triangle/archive/refs/tags/${tag_version}.tar.gz
+
 if [[ $? -ne 0 ]]; then
-    echo -e "${red}Ошибка загрузки Triangle, пожалуйста, убедитесь, что ваш сервер имеет доступ к Github ${plain}"
+    LOGE "Ошибка загрузки Triangle, пожалуйста, убедитесь, что ваш сервер имеет доступ к Github"
     exit 1
 fi
 
 tar zxvf ${tag_version}.tar.gz
 rm ${tag_version}.tar.gz -f
 
-echo -e "${green}Установка завершена...${plain}"
+dir_name=$(tar -tzf ${tag_version}.tar.gz | head -1 | sed -E 's|/.*||')
+mv ${dir_name}/* ${cur_dir}/
+rmdir ${dir_name}
+
+LOGI "Загрузка завершена..."
